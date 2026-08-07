95824326ee1321fdaa71c2d75d8d4530a106d728
Vinimapacfmap <araocris524@gmail.com>
2026-08-07 08:00:31 -0300

    feat(vinimap): Sincronização automática do código ViniMap Logistics OS

--- src/main.tsx
+++ src/main.tsx
@@ -5,41 +5,6 @@ import App from './App.tsx';
 import { ErrorBoundary } from './components/ErrorBoundary.tsx';
 import './index.css';
 
-// Enforce America/Sao_Paulo timezone for all Intl and Date formatting operations across the system
-const OriginalDateTimeFormat = Intl.DateTimeFormat;
-// @ts-ignore
-const CustomDateTimeFormat = function (this: any, locale?: string | string[], options?: any) {
-  const mergedOptions = { timeZone: 'America/Sao_Paulo', ...options };
-  if (new.target) {
-    return Reflect.construct(OriginalDateTimeFormat, [locale, mergedOptions], new.target);
-  }
-  return OriginalDateTimeFormat(locale, mergedOptions);
-};
-// Copy prototype and static properties to maintain 100% transparency
-CustomDateTimeFormat.prototype = OriginalDateTimeFormat.prototype;
-Object.setPrototypeOf(CustomDateTimeFormat, OriginalDateTimeFormat);
-Object.assign(CustomDateTimeFormat, OriginalDateTimeFormat);
-// @ts-ignore
-Intl.DateTimeFormat = CustomDateTimeFormat;
-
-const originalToLocaleString = Date.prototype.toLocaleString;
-// @ts-ignore
-Date.prototype.toLocaleString = function(this: Date, locales?: any, options?: any) {
-  return originalToLocaleString.call(this, locales || 'pt-BR', { timeZone: 'America/Sao_Paulo', ...options });
-};
-
-const originalToLocaleDateString = Date.prototype.toLocaleDateString;
-// @ts-ignore
-Date.prototype.toLocaleDateString = function(this: Date, locales?: any, options?: any) {
-  return originalToLocaleDateString.call(this, locales || 'pt-BR', { timeZone: 'America/Sao_Paulo', ...options });
-};
-
-const originalToLocaleTimeString = Date.prototype.toLocaleTimeString;
-// @ts-ignore
-Date.prototype.toLocaleTimeString = function(this: Date, locales?: any, options?: any) {
-  return originalToLocaleTimeString.call(this, locales || 'pt-BR', { timeZone: 'America/Sao_Paulo', ...options });
-};
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
@@ -47,3 +12,4 @@ createRoot(document.getElementById('root')!).render(
     </ErrorBoundary>
   </StrictMode>,
 );
+
